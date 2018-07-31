6362de8542f098ccfd104189f5faf08be49d6039
will <>
2018-07-31 16:50:15 +1200

    Voxel refactor

--- Assets/src/Terrian/Voxel.cs
+++ Assets/src/Terrian/Voxel.cs
@@ -3,8 +3,13 @@
     // WIP
     public struct VoxelValue
     {
+        // density of the voxel
         float value;
+        // color used to sample gradient
+        float colorVariation;
+        // object type
         int type;
+        // group it belongs to
         int groupIndex;
     }
 }
