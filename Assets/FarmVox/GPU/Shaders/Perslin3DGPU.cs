25f4555092220b464dde5394609c857bff8e774a
will <will@rushdigital.co.nz>
2019-01-04 14:00:56 +1300

    MINOR

--- Assets/FarmVox/GPU/Shaders/Perslin3DGPU.cs
+++ Assets/FarmVox/GPU/Shaders/Perslin3DGPU.cs
@@ -6,7 +6,6 @@ namespace FarmVox.GPU.Shaders
     public class Perlin3DGpu : IDisposable
     {
         private const int WorkGroups = 8;
-        private readonly int _size;
         private readonly Noise _noise;
         private readonly ComputeShader _shader;
         private readonly Vector3 _origin;
@@ -17,7 +16,6 @@ namespace FarmVox.GPU.Shaders
         public Perlin3DGpu(Noise noise, int size, Vector3 origin)
         {
             _noise = noise;
-            _size = size;
             _origin = origin;
             _shader = Resources.Load<ComputeShader>("Shaders/Perlin3D");
             _dataSize = size;
@@ -39,7 +37,6 @@ namespace FarmVox.GPU.Shaders
             _shader.SetBuffer(0, "_Results", Results);
             _shader.SetFloat("_Persistence", persistence);
             _shader.SetVector("_Origin", _origin);
-            _shader.SetInt("_Size", _size);
             _shader.SetInt("_Seed", seed);
             _shader.SetFloat("_Frequency", frequency);
             _shader.SetFloat("_Lacunarity", lacunarity);
@@ -50,7 +47,7 @@ namespace FarmVox.GPU.Shaders
             _shader.SetInt("_DataSize", _dataSize);
             _shader.SetInt("_Type", (int)_noise.Type);
 
-            var dispatchNum = Mathf.CeilToInt(_size / (float)WorkGroups);
+            var dispatchNum = Mathf.CeilToInt(_dataSize / (float)WorkGroups);
             _shader.Dispatch(0, dispatchNum, dispatchNum, dispatchNum);
         }
 
@@ -61,7 +58,7 @@ namespace FarmVox.GPU.Shaders
 
         public float[] Read()
         {
-            var data = new float[_size * _size * _size];
+            var data = new float[_dataSize * _dataSize * _dataSize];
             Results.GetData(data);
             return data;
         }
