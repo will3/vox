ea120137ab1e76816cd032e6bb3ac514e78eb754
will <will@rushdigital.co.nz>
2019-01-09 22:40:28 +1300

    MINOR

--- Assets/FarmVox/Terrain/Tree.cs
+++ Assets/FarmVox/Terrain/Tree.cs
@@ -5,14 +5,10 @@ namespace FarmVox.Terrain
 {
     public class Tree
     {
-        public readonly HashSet<Vector3Int> TrunkCoords;
-        public readonly HashSet<Vector3Int> StumpCoords;
         public readonly Vector3Int Pivot;
 
-        public Tree(HashSet<Vector3Int> stumpCoords, HashSet<Vector3Int> trunkCoords, Vector3Int pivot)
+        public Tree(Vector3Int pivot)
         {
-            StumpCoords = stumpCoords;
-            TrunkCoords = trunkCoords;
             Pivot = pivot;
         }
     }
