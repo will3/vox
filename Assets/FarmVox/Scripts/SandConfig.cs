312b0803d2370369009e805973dc226a4a49ea1a
will <will@rushdigital.co.nz>
2020-04-04 10:03:03 +1300

    MINOR

---
+++ Assets/FarmVox/Scripts/SandConfig.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FarmVox.Scripts
+{
+    [Serializable]
+    public class SandConfig
+    {
+        public Noise noise;
+        public ColorGradient colorGradient;
+        public ValueGradient heightGradient;
+    }
+}
